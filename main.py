b89aded53715900b2fd24af62ae9ec9250268569
SGVp15 <phosphorusp15@mail.ru>
2024-04-24 13:17:01 +0300

    delimiter=";"

--- main.py
+++ main.py
@@ -24,9 +24,12 @@ def str_x_to_dict(s: str) -> dict:
 
 
 def read_html_file(file_name: str):
-    with open(file_name, encoding='utf-8') as f:
-        s = f.read()
-    return s
+    try:
+        with open(file_name, encoding='utf-8') as f:
+            s = f.read()
+            return s
+    except FileNotFoundError:
+        return ''
 
 
 def download_sites(start, end):
@@ -34,23 +37,25 @@ def download_sites(start, end):
         url = url_base + str(i)
         r = requests.get(url)
         s = r.text.encode('utf-8')
-
-        with open(os.path.join(html_dir, f'{i}.html'), 'wb') as f:
-            f.write(s)
+        if r.status_code == 200:
+            with open(os.path.join(html_dir, f'{i}.html'), 'wb') as f:
+                f.write(s)
         print(f'download [ {i} ]')
 
 
 if __name__ == '__main__':
     start_game = 21829  # с какого номера скачать матчи
-    end_game = 21834  # по какой включительно
+    end_game = 21850  # по какой включительно
 
     download_sites(start_game, end_game)
 
     with open(export_csv_file, mode='w', encoding='utf-8', newline='') as f:
         is_write_header = False
         for i in range(start_game, end_game + 1):
-            print(i)
-            match: dict = parsing(read_html_file(os.path.join(html_dir, f'./{i}.html')))
+            match: dict = parsing(read_html_file(os.path.join(html_dir, f'{i}.html')))
+            if type(match) != dict:
+                print(f'[Error]\t{i}')
+                continue
             for k in match.keys():
                 for m in match.get(k):
                     w = csv.DictWriter(f, m.keys(), delimiter=";")
@@ -58,6 +63,7 @@ if __name__ == '__main__':
                         w.writeheader()
                         is_write_header = True
                     w.writerow(m)
+            print(f'[OK]\t{i}')
 
-    for file_name in range(start_game, end_game + 1):
-        os.remove(os.path.join(html_dir, str(file_name) + '.html'))  # удаление html файлов
+    # for file_name in range(start_game, end_game + 1):
+    #     os.remove(os.path.join(html_dir, str(file_name) + '.html'))  # удаление html файлов
